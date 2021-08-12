12121b775cf555a303476db1f73372582065ac45
Tijani Fulani <Fulani07@gmail.com>
2021-08-12 17:30:02 +0100

    ..

---
+++ utilities/mockData.js
@@ -0,0 +1,176 @@
+const designers = [
+  {
+    firstName: 'Jessica',
+    lastName: 'Wong',
+  },
+  {
+    firstName: 'James',
+    lastName: 'Maqueen',
+  },
+  {
+    firstName: 'Laura',
+    lastName: 'Valentino',
+  },
+  {
+    firstName: 'Giovanni',
+    lastName: 'Gabanna',
+  },
+  {
+    firstName: 'Paolo',
+    lastName: 'Boss',
+  },
+  {
+    firstName: 'Tatiana',
+    lastName: 'Chanel',
+  },
+  {
+    firstName: 'Stella',
+    lastName: 'Macroon',
+  },
+  {
+    firstName: 'Vegan',
+    lastName: 'Man',
+  },
+];
+
+const model = [
+  {
+    firstName: 'Valentina',
+    lastName: 'Italy',
+    agency: 'Best Of Italy',
+  },
+  {
+    firstName: 'Duncan',
+    lastName: 'Scotland',
+    agency: 'Best Of Scotland',
+  },
+  {
+    firstName: 'Tatiana',
+    lastName: 'Belarus',
+    agency: 'Best Of Belarus',
+  },
+  {
+    firstName: 'Elio',
+    lastName: 'Albania',
+    agency: 'Best Of Albania',
+  },
+  {
+    firstName: 'Jonathon',
+    lastName: 'England',
+    agency: 'Best Of England',
+  },
+  {
+    firstName: 'Sergio',
+    lastName: 'Portugal',
+    agency: 'Worse Of Portugal',
+  },
+  {
+    firstName: 'Steven',
+    lastName: 'Costa Rica',
+    agency: 'best Of Costa Rica',
+  },
+  {
+    firstName: 'Lars',
+    lastName: 'Norway',
+    agency: 'best Of Norway',
+  },
+];
+
+const events = [
+  {
+    name: 'Best Models 2021',
+    modelId: 1,
+    designerId: 1,
+    guestId: 1,
+  },
+  {
+    name: 'Best Summer Models',
+    modelId: 2,
+    designerId: 2,
+    guestId: 2,
+  },
+  {
+    name: 'Best Autumn Models',
+    modelId: 3,
+    designerId: 3,
+    guestId: 3,
+  },
+  {
+    name: 'Best Winter Models',
+    modelId: 4,
+    designerId: 4,
+    guestId: 4,
+  },
+  {
+    name: 'Best European Models',
+    modelId: 5,
+    designerId: 5,
+    guestId: 5,
+  },
+  {
+    name: 'Best American Models',
+    modelId: 6,
+    designerId: 6,
+    guestId: 6,
+  },
+  {
+    name: 'Best Asian Models',
+    modelId: 7,
+    designerId: 7,
+    guestId: 7,
+  },
+  {
+    name: 'Best African Models',
+    modelId: 8,
+    designerId: 8,
+    guestId: 8,
+  },
+  {
+    name: 'Best Super Models',
+    modelId: 8,
+    designerId: 8,
+    guestId: 8,
+  },
+  {
+    name: 'Best Lazy Models',
+    modelId: 7,
+    designerId: 7,
+    guestId: 7,
+  },
+  {
+    name: 'Best Sleepy Models',
+    modelId: 6,
+    designerId: 6,
+    guestId: 6,
+  },
+  {
+    name: 'Best Strong Models',
+    modelId: 5,
+    designerId: 5,
+    guestId: 5,
+  },
+  {
+    name: 'Best Weak Models',
+    modelId: 4,
+    designerId: 4,
+    guestId: 4,
+  },
+  {
+    name: 'Best Read Hair Models',
+    modelId: 3,
+    designerId: 3,
+    guestId: 3,
+  },
+  {
+    name: 'Best Black Hair Models',
+    modelId: 2,
+    designerId: 2,
+    guestId: 2,
+  },
+  {
+    name: 'Best Train Models',
+    modelId: 1,
+    designerId: 1,
+    guestId: 1,
+  },
+];
